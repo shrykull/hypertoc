e3412a2d277752a994c48ac44a97b3c1c3e78ea2
y.kurtovic <y.kurtovic@tup.com>
2014-11-04 11:03:51 +0100

    fancy animation when clicking the login button

--- loginscript.js
+++ loginscript.js
@@ -1,4 +1,42 @@
+var centerX,endY;
+$(document).ready(function(){
+ centerX = $(window).scrollLeft() + $(window).width() / 2 - ($("#pleaseWaitBox").width()/2);
+ endY = $(window).scrollTop() + $(window).height() / 4;
+});
+
+
 function login(){
-    alert("did you expect a working quenue system?");
-    
+
+    $("#pleaseWaitBox").css('visibility','visible').hide().fadeIn(700);
+    slideWaitingBoxUp( $(window).scrollTop() + $(window).height());
+
+
+/*    function jsonToServerTest(){
+      $.ajax({
+       type: "POST",
+       url: "server/enquePlayer.php",
+       data: {
+            username : "anonym"
+       }
+       }).done(function( serverResponse ) {
+       });
+
+    } */
+}
+
+function slideWaitingBoxUp(actY){
+  actY -= (actY-endY)*0.15;
+
+  $("#pleaseWaitBox").offset({ top: actY, left: centerX});
+  if(actY > endY +20){
+      setTimeout(function(){
+        slideWaitingBoxUp(actY)
+        }
+        ,33);
+  }else{
+    return;
+  }
+
+
+
 }
