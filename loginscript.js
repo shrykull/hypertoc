87a4852525c63a23adb5010ccc6981ac24969498
Yannick Kurtovic <yannick.kurtovic@gmx.net>
2015-04-25 20:38:37 +0200

    Polling started

--- loginscript.js
+++ loginscript.js
@@ -1,29 +1,26 @@
 var centerX,endY;
 $(document).ready(function(){
- centerX = $(window).scrollLeft() + $(window).width() / 2 - ($("#pleaseWaitBox").width()/2);
- endY = $(window).scrollTop() + $(window).height() / 5;
- $("#pleaseWaitBox").offset({top: $(window).scrollTop() + $(window).height(),left: centerX});
+  centerX = $(window).scrollLeft() + $(window).width() / 2 - ($("#pleaseWaitBox").width()/2);
+  endY = $(window).scrollTop() + $(window).height() / 5;
+  $("#pleaseWaitBox").offset({top: $(window).scrollTop() + $(window).height(),left: centerX});
 });
 
-
 function login(){
-
-    $("#pleaseWaitBox").css('visibility','visible').hide().fadeIn(200);
-    slideWaitingBoxUp( $(window).scrollTop() + $(window).height());
-    $("#loginAnonymousButton").fadeOut(100);
-
-
+  pollDataFromServer()
+  $("#pleaseWaitBox").css('visibility','visible').hide().fadeIn(500);
+  slideWaitingBoxUp( $(window).scrollTop() + $(window).height());
+  $("#loginAnonymousButton").fadeOut(100);
 }
 
 function slideWaitingBoxUp(actY){
-  actY -= (actY-endY)*0.15;
+  actY -= (actY-endY)*0.2;
 
   $("#pleaseWaitBox").offset({ top: actY, left: centerX});
   if(actY > endY +20){
-      setTimeout(function(){
-        slideWaitingBoxUp(actY)
-        }
-        ,33);
+    setTimeout(function(){
+      slideWaitingBoxUp(actY)
+    }
+    ,33);
   }else{
     return;
   }
@@ -31,12 +28,37 @@ function slideWaitingBoxUp(actY){
 
 function enquePlayer(){
   $.ajax({
-   type: "POST",
-   url: "server/enquePlayer.php",
-   data: {
-        username : "anonym"
-   }
-   }).done(function( serverResponse ) {
-   });
+    type: "POST",
+    url: "server/enquePlayer.php",
+    data: {
+      username : "anonym"
+    }
+  }).done(function( serverResponse ) {
+  });
 
+}
+
+//Examle for rest
+function pollDataFromServer(){
+
+  $.ajax({
+    url: "http://rest-service.guides.spring.io/greeting",
+  }).done(function( serverResponse ) {
+
+    ajaxDone(serverResponse);
+
+    setTimeout(function(){
+      pollDataFromServer();
+    },1000);
+
+  });
+  //TODO: hier pollen bis entweder fehler oder eben spieler gefunden
+
+}
+function ajaxDone(response){
+  console.log(response);
+  $("#pleaseWaitBox").append("<br/>" + response.id + "</br>");
+  $("#pleaseWaitBox").append(response.content);
+  $('#pleaseWaitBox').append("<p>irgendwie status?</p>");
+  //TODO: wenn kein Fehler zurück dann hald son polling aktivieren!
 }
