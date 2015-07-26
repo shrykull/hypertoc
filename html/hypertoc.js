4639e175f07629303af57b746adc03d08b16633d
shrykull <shrykull@shrye.net>
2015-07-26 19:14:53 +0200

    fully implemented board and symbol drawing functions

--- html/hypertoc.js
+++ html/hypertoc.js
@@ -11,9 +11,12 @@ hypertoc.controller('mainContentCtrl', function($scope, BoardUIService) {
 
 hypertoc.factory('BoardUIService', function(GameDataService, InputEventService, DrawService) {   
   var drawFunction = function() {
-    DrawService.drawBoardDecorations();
-    DrawService.drawBoardSymbols(GameDataService.board);
-    DrawService.drawBoardShape();
+    DrawService.drawBoardDecorations(); //draws outer border
+    
+    DrawService.initializeDynamicBoardMarkers(); //draws all X, O to be selected for visibility later on
+    
+    DrawService.drawBoardShapes(); //draws #
+    DrawService.drawBoardSymbols(GameDataService.getBoard()); //applies visibility rules for all x and o according to board
   };
   return {
     draw:drawFunction, 
@@ -71,18 +74,25 @@ hypertoc.factory('DrawService', function() {
   
   var marginPercentage = 0.01;
   
+  var strokemargin = marginPercentage * width;
+  
   /*
    * calculated simplifiers for use later (add more as needed)
    */
   var margin = {
-                top:height * marginPercentage, 
-                bottom:height * marginPercentage, 
-                left: width * marginPercentage, 
-                right: width * marginPercentage};
+    top:height * marginPercentage, 
+    bottom:height * marginPercentage, 
+    left: width * marginPercentage, 
+    right: width * marginPercentage
+  };
+  
 
-  var subfieldwidth  = (width  - (margin.left + margin.right  + marginPercentage * width  * 2)) / 3,
-      subfieldheight = (height - (margin.top  + margin.bottom + marginPercentage * height * 2)) / 3;
+  var subfieldwidth  = (width  - (margin.left*4 + margin.right*4)) / 3,
+      subfieldheight = (height - (margin.top*4  + margin.bottom*4)) / 3;
 
+  var symbolWidth   = (subfieldwidth  - (margin.left*4 + margin.right*4))  / 3;
+  var symbolHeight  = (subfieldheight - (margin.left*4 + margin.right*4))  / 3;
+    
   var gameView = d3.select('#gameContainer')
     .append('svg')
     .attr("width", width)
@@ -117,44 +127,106 @@ hypertoc.factory('DrawService', function() {
   
   var generateActiveSubfieldMarkers = function() {
     var xpos, ypos, absoluteXpos, absoluteYpos;
+    var subfield;
     
-    for (var i = 0; i < 10; ++i) {
-        xpos = i % 3;
-        ypos = Math.floor(i / 3);
-        absoluteXpos = margin.left + marginPercentage * width * xpos + xpos * subfieldwidth;
-        absoluteYpos = margin.top + marginPercentage * height * ypos + ypos * subfieldheight;
-        gameView
-        .append("g") //generate a group with id to find it again once we get data
-            .attr("id", "subfield" + i)
-            .attr("transform", "translate("+ absoluteXpos + "," + absoluteYpos + ")")
-        .append("rect")
-            .attr("x", 0)
-            .attr("y", 0)
-            .attr("width", subfieldwidth)
-            .attr("height", subfieldheight)
-            .attr("class", "gameViewMarkedSubfield")
-            .attr("visibility", "hidden"); //default visibility is hidden, we'll activate it once needed
+    var currentMarkerGroup;
+    var linearSymbolIndex; //needed for translation from coordinates to array index
+    
+    for (var linearSubfieldIndex = 0; linearSubfieldIndex < 9; ++linearSubfieldIndex) {
+      xpos = linearSubfieldIndex % 3;
+      ypos = Math.floor(linearSubfieldIndex / 3);
+      absoluteXpos = margin.left  + (margin.left *2   + margin.right  + subfieldwidth)  * xpos;
+      absoluteYpos = margin.top   + (margin.bottom *2 + margin.top    + subfieldheight) * ypos ;
+      subfield = gameView.append("g") //generate a group with id to find it again once we get data
+        .attr("id", "subfield-" + linearSubfieldIndex)
+        .attr("transform", "translate("+ absoluteXpos + ", " + absoluteYpos + ")");
+      subfield.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", subfieldwidth)
+        .attr("height", subfieldheight)
+        .attr("class", "gameViewMarkedSubfield")
+        .attr("visibility", "hidden"); //default visibility is hidden, we'll activate it once needed
+      
+      var x;
+      for (var subfieldIndexX = 0; subfieldIndexX < 3; ++subfieldIndexX) {
+        for (var subfieldIndexY = 0; subfieldIndexY < 3; ++subfieldIndexY) {
+          absoluteXpos = margin.left  + (margin.left*2  + margin.right  + symbolWidth)  * subfieldIndexX;
+          absoluteYpos = margin.top   + (margin.top *2  + margin.bottom + symbolHeight) * subfieldIndexY;
+          linearSymbolIndex = subfieldIndexX + subfieldIndexY * 3;
+          
+          currentMarkerGroup = subfield.append("g").attr("id", "symbol-" + linearSymbolIndex);
+          currentMarkerGroup.attr("transform", "translate(" + absoluteXpos + ", " + absoluteYpos + ")");
+          x = currentMarkerGroup.append("g");
+          x.attr("class", "marker marker-x")
+            .attr("visibility", "hidden")
+            .append("line")
+              .attr("x1", 0).attr("y1", 0)
+              .attr("x2", symbolWidth)
+              .attr("y2", symbolHeight);
+          x.append("line")
+              .attr("x1", symbolWidth).attr("y1", 0)
+              .attr("x2", 0)
+              .attr("y2", symbolHeight);
+          currentMarkerGroup.append("g")
+            .attr("class", "marker marker-o")
+            .attr("visibility", "hidden")
+            .append("circle")
+              .attr("cx", symbolWidth / 2)
+              .attr("cy", symbolHeight / 2)
+              .attr("r", Math.min(symbolWidth, symbolHeight) / 2);
+         }
+      }
     }
-  }
+  };
+  
+  var drawX = function(subfieldIndex, symbolIndex) {
+    d3.select('#subfield-' + subfieldIndex)
+      .select('#symbol-' + symbolIndex)
+      .select('.marker-x').attr("visibility", "visible");
+      
+    d3.select('#subfield-' + subfieldIndex)
+      .select('#symbol-' + symbolIndex)
+      .select('.marker-o').attr("visibility", "hidden");
+  };
+  var drawO = function(subfieldIndex, symbolIndex) {
+    d3.select('#subfield-' + subfieldIndex)
+      .select('#symbol-' + symbolIndex)
+      .select('.marker-x').attr("visibility", "hidden");
+      
+    d3.select('#subfield-' + subfieldIndex)
+      .select('#symbol-' + symbolIndex)
+      .select('.marker-o').attr("visibility", "visible");
+  };
+  var drawEmptySpace = function(subfieldIndex, symbolIndex) {
+    d3.select('#subfield-' + subfieldIndex)
+      .select('#symbol-' + symbolIndex)
+      .select('.marker-x').attr("visibility", "hidden");
+      
+    d3.select('#subfield-' + subfieldIndex)
+      .select('#symbol-' + symbolIndex)
+      .select('.marker-o').attr("visibility", "hidden");
+  };
   return {
-    drawBoardShape: function() {
-      //generate groups with unique IDs to draw our stuff on later on.
+    initializeDynamicBoardMarkers: function() {
+      //generate groups with unique IDs to toggle visibility later on
       generateActiveSubfieldMarkers();
-     
+    },
+    drawBoardShapes: function() {     
       var boardShape = gameView.append("g")
         .attr("id", "gameViewBoardShapes");
       //draw "big" boardShape
-      drawGenericBoardshape(boardShape, "boardShapeMain", margin.left, margin.top, width - margin.left, height - margin.bottom);
+      drawGenericBoardshape(boardShape, "boardShapeMain", margin.left, margin.top, width - margin.right, height - margin.bottom);
 
       var smallBoardWidth = (width - marginPercentage * 6 * width) / 3;
       var smallBoardHeight = (height - marginPercentage * 6 * height) / 3;
       for (var i = 0; i < 3; ++i) {
         for (var o = 0; o < 3; ++o) {
           drawGenericBoardshape(boardShape, "boardShapeSmall", 
-            margin.left * (i + 3) + smallBoardWidth  * i, 
-            margin.top  * (o + 3) + smallBoardHeight * o, 
-            (smallBoardWidth  * (i+1)) + (marginPercentage * width * (i+1)),
-            (smallBoardHeight * (o+1)) + (marginPercentage * height * (o+1))
+            margin.left + (margin.left + margin.right  + smallBoardWidth ) * i,
+            margin.top  + (margin.top  + margin.bottom + smallBoardHeight) * o, 
+            margin.left + (margin.left + margin.right  + smallBoardWidth ) * i + smallBoardWidth,
+            margin.top  + (margin.top  + margin.bottom + smallBoardHeight) * o + smallBoardHeight
           );
         }
       }
@@ -163,32 +235,48 @@ hypertoc.factory('DrawService', function() {
       var boardDecoration = gameView.append("g");
       boardDecoration.attr("id", "gameViewBoarddecoration");
       var line = boardDecoration.append("line")
-      .attr("class", "boardDecorationOuter")
-        .attr("x1", margin.left)
-        .attr("y1", margin.top)
-        .attr("x2", margin.left)
-        .attr("y2", height - margin.bottom);
-      line = boardDecoration.append("line")
+      .attr("class", "boardDecorationOuter") //left margin
+        .attr("x1", margin.left / 2)
+        .attr("y1", margin.top / 2)
+        .attr("x2", margin.left / 2)
+        .attr("y2", height - margin.bottom / 2);
+      line = boardDecoration.append("line") //top margin
         .attr("class", "boardDecorationOuter")
-        .attr("x1", margin.left)
-        .attr("y1", margin.top)
-        .attr("x2", width - margin.right)
-        .attr("y2", margin.top);
-      line = boardDecoration.append("line")
+        .attr("x1", margin.left / 2)
+        .attr("y1", margin.top / 2)
+        .attr("x2", width - margin.right / 2)
+        .attr("y2", margin.top / 2);
+      line = boardDecoration.append("line") //right margin
         .attr("class", "boardDecorationOuter")
-        .attr("x1", width - margin.right)
-        .attr("y1", margin.top)
-        .attr("x2", width - margin.right)
-        .attr("y2", height - margin.bottom);
-      line = boardDecoration.append("line")
+        .attr("x1", width - margin.right / 2)
+        .attr("y1", margin.top / 2)
+        .attr("x2", width - margin.right / 2)
+        .attr("y2", height - margin.bottom / 2);
+      line = boardDecoration.append("line") //bottom margin
         .attr("class", "boardDecorationOuter")
-        .attr("x1", margin.left)
-        .attr("y1", height - margin.bottom)
-        .attr("x2", width - margin.right)
-        .attr("y2", height - margin.bottom);
+        .attr("x1", margin.left / 2)
+        .attr("y1", height - margin.bottom / 2)
+        .attr("x2", width - margin.right / 2)
+        .attr("y2", height - margin.bottom / 2);
     },
     drawBoardSymbols: function(board) {
-        //draw symbols into those groups
+        //draw symbols into subfields 0-8
+        var subfieldIndex, symbolIndex;
+        for (subfieldIndex = 0; subfieldIndex < 9; ++subfieldIndex) {
+            for (symbolIndex = 0; symbolIndex < 9; ++symbolIndex) {
+                switch (board.field[subfieldIndex][symbolIndex]) {
+                    case "X":
+                        drawX(subfieldIndex, symbolIndex);
+                        break;
+                    case "O":
+                        drawO(subfieldIndex, symbolIndex);
+                        break;
+                    default: //make sure nothing is drawn in the selected group
+                        drawEmptySpace(subfieldIndex, symbolIndex);
+                }
+            }
+        }
+        
     }
    };
 });
